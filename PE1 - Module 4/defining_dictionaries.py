f45d01bff8c95eca46a5ce44d1b53b68515dd692
Augusto Alonso de la Cruz Jimenez <augusto.delacruz@banregio.com>
2024-03-13 23:46:17 -0600

    introduction to tuples and dictionaries

---
+++ PE1 - Module 4/defining_dictionaries.py
@@ -0,0 +1,50 @@
+dictionary = {"book": "libro", "chair": "silla", "box": "caja"}
+totals_by_status = {"backlog": 45, "in progress": 12, "done": 40}
+empty_dict = {}
+
+print(dictionary)
+print(totals_by_status)
+print(empty_dict)
+
+# To get a value, we deliver a valid key
+# We can use single or double quotes to specify the key
+print("Book in spanish is:", dictionary['book'])
+print("Chair in spanish is:", dictionary["chair"])
+print("The total tasks in done status are: ", totals_by_status["done"])
+
+# If we use an invalid key, we get the error:
+# print("Mouse in spanish is:", dictionary['mouse'])
+# KeyError: 'mouse'
+
+# Every dictionary have a key(method)
+print("Dictionary keys:", dictionary.keys())
+print("Status:", totals_by_status.keys())
+
+animals = {
+    "cat": "chat",
+    "dog": "chien",
+    "horse": "cheval"
+    }
+
+
+for key in animals.keys():
+    print(key, "->", animals[key])
+
+# If we want to navigate the keys in an ordered way, we can enrich the for loop
+for key in sorted(animals.keys()):
+    print(key, "->", animals[key])
+
+# We can extract key value pairs using the method items() from each dictionary
+for status, total in totals_by_status.items():
+    print("Status:", status, "->", "Total:", total)
+
+# Assigning a new value for an existing key
+totals_by_status["backlog"] = 35
+totals_by_status["in progress"] = 10
+totals_by_status["done"] = 52
+print(totals_by_status)
+
+# Adding a non existing key (and value)}
+totals_by_status["on hold"] = 5
+print(totals_by_status)
+
---
+++ PE1 - Module 4/defining_tuples.py
@@ -0,0 +1,35 @@
+# The preferred way to declare a tuple is using parentheses
+tuple_1 = (1, 2, 3, 4, 5)
+
+# It's also posibble to create a tuple just from a set of values
+# sepatated by commas
+tuple_2 = 1, 2, 3, 4, 5
+
+# If we print out both tuples, this is the result
+# (1, 2, 3, 4, 5) (1, 2, 3, 4, 5)
+print(tuple_1, tuple_2)
+
+# We are able to create an empty tuple. In this case, parentheses are necessary
+tuple_3 = ()
+print("Tuple 3:", tuple_3)
+
+# If we want to create a tuple with a single element, we need to end
+# the unique value with a comma. If we don't do so, we will get a single
+# variable instead of a tuple
+tuple_4 = (1,)
+tuple_5 = 1,
+print("Tuple 4 type is:", type(tuple_4))
+print("Tuple 5 type is:", type(tuple_5))
+
+# Without the comma at the end, they are simple variable and values
+tuple_6 = (1)
+print("Tuple 6 type is:", type(tuple_6))
+
+# Don't modify a tuple element
+# tuple_1[0] = 0
+# TypeError: 'tuple' object does not support item assignment
+
+# If we try to delete an element of the tuple
+# del my_tuple[0]
+# TypeError: 'tuple' object doesn't support item deletion
+
